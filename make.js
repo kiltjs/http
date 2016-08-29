1ee7fef7473dad8b12afe6ace4d37b2092121b0d
Jesús Germade <jesus@germade.es>
2016-08-29 18:27:47 +0200

    whoops :sweat-smile:

--- make.js
+++ make.js
@@ -44,7 +44,7 @@ require('nitro')(function (nitro) {
   });
 
   nitro.task('gh-release', function () {
-    nitro.github.release( 'v' + require('../package').version, { branch: 'release' });
+    nitro.github.release( 'v' + require('./package').version, { branch: 'release' });
   });
 
 }).run();
